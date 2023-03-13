1d1ceced6015e86e52e25194b33b2845ddcb1cc5
kesavan <kesavanplk7@gmail.com>
2023-03-13 22:01:33 +0530

    add script logic

--- script/script.js
+++ script/script.js
@@ -1,5 +1,32 @@
 // elements
-const hour =document.getElementById('hour');
-const min =document.getElementById('min');
-const sec =document.getElementById('sec');
+const clockHour =document.getElementById('hour');
+const clockMin =document.getElementById('min');
+const clockSec =document.getElementById('sec');
+
+console.log(hour);
+//global variables
+
+//functions
+setInterval(() => {
+   const todayDate = new Date();
+
+   let hour =todayDate.getHours();
+   let min =todayDate.getMinutes();
+   let sec =todayDate.getSeconds();
+   
+
+if (hour < 10 ){
+    hour = "0" + hour;
+}
+if (min < 10 ){
+    min ="0" + min;
+}
+if (sec < 10 ){
+    sec = "0" + sec;
+}
+
+    clockHour.innerText=hour;
+    clockMin.innerText=min;
+    clockSec.innerText=sec;
+}, 1000);
 
